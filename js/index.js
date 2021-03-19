0843875e10cb97631f2687674c2d246a7a93f839
Benito <dcostabenoit@gmail.com>
2021-03-19 13:34:04 +0100

    1ère version Formuluaire

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
-function sendMail(params) {
-    var temp
+function sendMail(event) {
+  event.preventDefault();
+  const tempParams = {
+    name: document.querySelector('#name').value,
+    email: document.querySelector('#email').value,
+    category: document.querySelector('#dropdown').value,
+    object: document.querySelector('#object').value,
+    message: document.querySelector('#comments').value,
+  };
+
+
+  const msgSend = document.querySelector("#msgSend");
+
+  emailjs.send('service_uqrkym9', 'template_oazxygk', tempParams).then(function (res) {
+    console.log("success", res.status);
+    msgSend.innerHTML = 'Votre message à bien été envoyé';
+    msgSend.style.color = "green";
+  })
 }
