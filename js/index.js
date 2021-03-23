64fea10fc20abdcb0e786cf799da772c47453f2e
Paul-Emmanuel Legrand <paulm.legrand@gmail.com>
2021-03-23 16:59:57 +0100

    changing style of index.html

--- js/index.js
+++ js/index.js
@@ -1,29 +1,19 @@
-function sendMail(event) {
-  event.preventDefault();
-  const tempParams = {
-    name: document.querySelector('#name').value,
-    email: document.querySelector('#email').value,
-    category: document.querySelector('#dropdown').value,
-    object: document.querySelector('#object').value,
-    message: document.querySelector('#comments').value,
-  };
+// // function sendMail(event) {
+//   event.preventDefault();
+//   const tempParams = {
+//     name: document.querySelector('#name').value,
+//     email: document.querySelector('#email').value,
+//     category: document.querySelector('#dropdown').value,
+//     object: document.querySelector('#object').value,
+//     message: document.querySelector('#comments').value,
+//   };
 
 
-  const msgSend = document.querySelector("#msgSend");
+//   const msgSend = document.querySelector("#msgSend");
 
-  emailjs.send('service_uqrkym9', 'template_oazxygk', tempParams).then(function (res) {
-    console.log("success", res.status);
-    msgSend.innerHTML = 'Votre message à bien été envoyé';
-    msgSend.style.color = "green";
-  })
-}
+//   emailjs.send('service_uqrkym9', 'template_oazxygk', tempParams).then(function (res) {
+//     console.log("success", res.status);
+//     msgSend.innerHTML = 'Votre message à bien été envoyé';
+//     msgSend.style.color = "green";
+//   })
 
-const checkbox = document.querySelector('.my-form input[type="checkbox"]');
-const btns = document.querySelectorAll(".my-form button");
-
-checkbox.addEventListener("change", function() {
-  const checked = this.checked;
-  for (const btn of btns) {
-    checked ? (btn.disabled = false) : (btn.disabled = true);
-  }
-});
