41a09577db11710082f4762c526cd750435e76f5
Paul-Emmanuel Legrand <paulm.legrand@gmail.com>
2021-03-24 21:09:49 +0100

    working on css

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
-// // function sendMail(event) {
-//   event.preventDefault();
-//   const tempParams = {
-//     name: document.querySelector('#name').value,
-//     email: document.querySelector('#email').value,
-//     category: document.querySelector('#dropdown').value,
-//     object: document.querySelector('#object').value,
-//     message: document.querySelector('#comments').value,
-//   };
+// function sendMail(event) {
+  event.preventDefault();
+  const tempParams = {
+    name: document.querySelector('#name').value,
+    email: document.querySelector('#email').value,
+    category: document.querySelector('#dropdown').value,
+    object: document.querySelector('#object').value,
+    message: document.querySelector('#comments').value,
+  };
 
 
-//   const msgSend = document.querySelector("#msgSend");
+  const msgSend = document.querySelector("#msgSend");
 
-//   emailjs.send('service_uqrkym9', 'template_oazxygk', tempParams).then(function (res) {
-//     console.log("success", res.status);
-//     msgSend.innerHTML = 'Votre message à bien été envoyé';
-//     msgSend.style.color = "green";
-//   })
+  emailjs.send('service_uqrkym9', 'template_oazxygk', tempParams).then(function (res) {
+    console.log("success", res.status);
+    msgSend.innerHTML = 'Votre message à bien été envoyé';
+    msgSend.style.color = "green";
+  })
 
