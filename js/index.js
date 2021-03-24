3dc9bc7fe134dfb3f65460192b8604ded5ece8d1
Paul-Emmanuel Legrand <paulm.legrand@gmail.com>
2021-03-24 23:17:30 +0100

    working index

--- js/index.js
+++ js/index.js
@@ -1,19 +1,16 @@
-// function sendMail(event) {
-  event.preventDefault();
+document.getElementById("sendmail").addEventListener("click",function(event){
+event.preventDefault();
+document.querySelector(".my-form").reset();
+})
+
+function sendMail(params) {
   const tempParams = {
-    name: document.querySelector('#name').value,
-    email: document.querySelector('#email').value,
-    category: document.querySelector('#dropdown').value,
-    object: document.querySelector('#object').value,
-    message: document.querySelector('#comments').value,
+    name: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    message: document.getElementById('message').value,
   };
 
-
-  const msgSend = document.querySelector("#msgSend");
-
-  emailjs.send('service_uqrkym9', 'template_oazxygk', tempParams).then(function (res) {
+  emailjs.send('parisEscapePlan', 'PEP', tempParams).then(function (res) {
     console.log("success", res.status);
-    msgSend.innerHTML = 'Votre message à bien été envoyé';
-    msgSend.style.color = "green";
-  })
+  })}
 
