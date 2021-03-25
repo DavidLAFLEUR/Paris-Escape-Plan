38b3f88ab81329102573a3cda8c3c979eedc81e9
bekri78 <bekri.mehdi@outlook.com>
2021-03-25 15:49:09 +0100

    change folder img,  delete loader , show all card plan

--- js/page1.js
+++ js/page1.js
@@ -1,14 +1,23 @@
 // function filtre card
 function admSelectCheck(nameSelect) {
+  console.log(nameSelect.value);
+  // si selection
+  var preElements = document.getElementsByClassName("prettyprint");
   if (nameSelect) {
-    var preElements = document.getElementsByClassName("prettyprint");
-    for (var i = 0; i < preElements.length; i++) {
-      //si la class prettyprint contiend le selected value ? diplsay none ou block
-      preElements[i].style.display = preElements[i].classList.contains(
-        nameSelect.value
-      )
-        ? "block"
-        : "none";
+    if (nameSelect.value == "start") {
+      // reaparition des article
+      for (var i = 0; i < preElements.length; i++) {
+        preElements[i].style.display = "block";
+      }
+    } else {
+      for (var i = 0; i < preElements.length; i++) {
+        //si la class prettyprint contiend la valeur du select diplsay none ou block
+        preElements[i].style.display = preElements[i].classList.contains(
+          nameSelect.value
+        )
+          ? "block"
+          : "none";
+      }
     }
   }
 }
@@ -30,19 +39,21 @@ const mars =
   "<div style='float:left'><img class ='imgCarte' src='../ImgNature/champsDeMars.jpg'></div><div style='float:right; padding: 10px;'><b>Champs de Mars</b><br/>Un vaste jardin public, entièrement ouvert  <br/> Ville, Paris</div>";
 const jardinLuxembourg =
   "<div style='float:left'><img  class ='imgCarte' src='../ImgNature/luxembourf.jpg'></div><div style='float:right; padding: 10px;'><b>Jardin du Luxembourg</b><br/>Horaire de fermeture du Jardin du Luxembourg. <br/>Pendant la durée du couvre-feu à 18h, <br/>l'heure de fermeture du jardin est maintenue à 17h30.<br/> Ville, Paris</div>";
+const boulogne =
+  "<div style='float:left'><img  class ='imgCarte' src='../ImgNature/boulogne'></div><div style='float:right; padding: 10px;'><b>Bois de Boulogne</b><br/>Le bois de Boulogne est une étendue boisée <br/>située dans le 16 arrondissement <br/>Couvrant une superficie de 846 hectares .<br/> Ville, Paris</div>";
 
 //DETENTE ->.
 
 const dansLeNoir =
-  "<div style='float:left'><img class='imgCarte' src='../ImgMarker/dansLeNoir.jpg'></div><div style='float:right; padding: 10px;'><b>Séance dans le Noir</b><br/> Un lieu insolite, au cœur de Paris, loin du rythme trépidant de la ville. <br/>Les massages sont faits sur-mesure  <br/> les praticiens s'adaptent constamment aux demandes des clients <br/> Ville, Paris</div>";
+  "<div style='float:left'><img class='imgCarte' src='../ImgDetente/dansLeNoir.jpg'></div><div style='float:right; padding: 10px;'><b>Séance dans le Noir</b><br/> Un lieu insolite, au cœur de Paris, loin du rythme trépidant de la ville. <br/>Les massages sont faits sur-mesure  <br/> les praticiens s'adaptent constamment aux demandes des clients <br/> Ville, Paris</div>";
 const bulleFLottante =
-  "<div style='float:left'><img class='imgCarte' src='../ImgMarker/flottaison.jpg'></div><div style='float:right; padding: 10px;'><b>Bulle Flottante</b><br/>Relaxation, méditation, bien-êtreTentez l'expérience!<br/> Déconnectez-vous, récupérez physiquement  <br/>pour découvrir la fabuleuse bulle flottante<br/> Ville, Paris</div>";
+  "<div style='float:left'><img class='imgCarte' src='../ImgDetente/flottaison.jpg'></div><div style='float:right; padding: 10px;'><b>Bulle Flottante</b><br/>Relaxation, méditation, bien-êtreTentez l'expérience!<br/> Déconnectez-vous, récupérez physiquement  <br/>pour découvrir la fabuleuse bulle flottante<br/> Ville, Paris</div>";
 const barZen =
-  "<div style='float:left'><img class ='imgCarte' src='../ImgMarker/sieste.jpg'></div><div style='float:right; padding: 10px;'><b>Bar Zen</b><br/> Matinée de folie, coup de barre après manger  <br/> et le sentiment que l’on ne passera jamais l’après-midi <br/> si on ne ferme pas les yeux 10 min, ça vous parle ?  <br/> Déconnectez-vous. Libérez votre esprit. <br/> Ville, Paris</div>";
+  "<div style='float:left'><img class ='imgCarte' src='../ImgDetente/sieste.jpg'></div><div style='float:right; padding: 10px;'><b>Bar Zen</b><br/> Matinée de folie, coup de barre après manger  <br/> et le sentiment que l’on ne passera jamais l’après-midi <br/> si on ne ferme pas les yeux 10 min, ça vous parle ?  <br/> Déconnectez-vous. Libérez votre esprit. <br/> Ville, Paris</div>";
 const brocante =
-  "<div style='float:left'><img class ='imgCarte' src='../ImgMarker/brocante.jpg'></div><div style='float:right; padding: 10px;'><b>Brocante</b><br/>Brocantes, vide-greniers et marchés éphémères<br/> Paris vous donnent envie ?<br/> Ville, Paris</div>";
+  "<div style='float:left'><img class ='imgCarte' src='../ImgDetente/brocante.jpg'></div><div style='float:right; padding: 10px;'><b>Brocante</b><br/>Brocantes, vide-greniers et marchés éphémères<br/> Paris vous donnent envie ?<br/> Ville, Paris</div>";
 const jacuzzi =
-  "<div style='float:left'><img class ='imgCarte' src='../ImgMarker/jacuzzi.jpg'></div><div style='float:right; padding: 10px;'><b>Jacuzzi</b><br/>Un jacuzzi privatif, un sauna rien que pour vous,<br/> un bar privé avec réfrigérateur et un lit de plage, <br/>le tout dans l’ambiance de votre choix. <br/> Ville, Paris</div>";
+  "<div style='float:left'><img class ='imgCarte' src='../ImgDetente/jacuzzi.jpg'></div><div style='float:right; padding: 10px;'><b>Jacuzzi</b><br/>Un jacuzzi privatif, un sauna rien que pour vous,<br/> un bar privé avec réfrigérateur et un lit de plage, <br/>le tout dans l’ambiance de votre choix. <br/> Ville, Paris</div>";
 
 // FOOD ->
 
@@ -56,29 +67,34 @@ const grille =
   "<div style='float:left'><img class ='imgCarte' src='../ImgFood/food4.jpg''></div><div style='float:right; padding: 10px;'><b>Avec leur grillé</b><br/> Fred Peneau et Marie Carcassone   <br/>redorent clairement le blason <br/>du salade-tomate- oignon  <br/> Ville, Paris</div>";
 const adar =
   "<div style='float:left'><img class ='imgCarte' src='../ImgFood/resto.jpeg''></div><div style='float:right; padding: 10px;'><b>Restaurant Adar</b><br/> Le Passage des Panoramas, nouveau <br/>passage obligé pour les jeunes chefs stars ?  <br/>Ultime bon plan :la maison fait <br/>aussi des sandwichs à emporter. <br/> Ville, Paris</div>";
+const sezame =
+  "<div style='float:left'><img class ='imgCarte' src='../ImgFood/sezame.jpeg''></div><div style='float:right; padding: 10px;'><b>Restaurant sezame</b><br/> Nous prenons les mesures nécessaires <br/>afin de maintenir la livraison de vos plats préférés <br/>dans les meilleures conditions de protection<br/>pour vous <br/> Ville, Paris</div>";
 
 //CULTURE -->
 
 const parfum =
-  "<div style='float:left'><img class ='imgCarte' src='../ImgMarker/museeParfum.jpg'></div><div style='float:right; padding: 10px;'><b>Musée Parfum</b><br/> Inédit à Paris, le Musée du Parfum  <br/>propose un concept muséal unique et expose de manière didactique toutes <br/> les étapes  <br/> qui donnent vie à ce mythique objet de luxe et objet de notre quotidien . <br/> Ville, Paris</div>";
+  "<div style='float:left'><img class ='imgCarte' src='../ImgCulture/museeParfum.jpg'></div><div style='float:right; padding: 10px;'><b>Musée Parfum</b><br/> Inédit à Paris, le Musée du Parfum  <br/>propose un concept muséal unique et expose de manière didactique toutes <br/> les étapes  <br/> qui donnent vie à ce mythique objet de luxe et objet de notre quotidien . <br/> Ville, Paris</div>";
 const gallery =
-  "<div style='float:left'><img class ='imgCarte' src='../ImgMarker/muséeCulture.jpg'></div><div style='float:right; padding: 10px;'><b>Expostion</b><br/> Pour la première exposition   <br/> orchestrée dans ce lieu de 800 m2 <br/> Ville, Paris</div>";
+  "<div style='float:left'><img class ='imgCarte' src='../ImgCulture/muséeCulture.jpg'></div><div style='float:right; padding: 10px;'><b>Expostion</b><br/> Pour la première exposition   <br/> orchestrée dans ce lieu de 800 m2 <br/> Ville, Paris</div>";
 const streetArt =
-  "<div style='float:left'><img class ='imgCarte' src='../ImgMarker/streetArt.jpg'></div><div style='float:right; padding: 10px;'><b>Street Art Exposition</b><br/>Au sud de Paris, le 13e arrondissement <br/> regorge de fresques de street art. <br/> Ville, Paris</div>";
+  "<div style='float:left'><img class ='imgCarte' src='../ImgCulture/streetArt.jpg'></div><div style='float:right; padding: 10px;'><b>Street Art Exposition</b><br/>Au sud de Paris, le 13e arrondissement <br/> regorge de fresques de street art. <br/> Ville, Paris</div>";
 const jardinDesPlantes =
-  "<div style='float:left'><img class ='imgCarte' src='../ImgMarker/jardinDesPlante.jpg'></div><div style='float:right; padding: 10px;'><b>Jardin des Plantes </b><br/> Jardin alpin, écologique, roseraie,   <br/>Partez à la découverte des onze jardins<br/> qui composent un Jardin des Plantes unique  <br/> Ville, Paris</div>";
+  "<div style='float:left'><img class ='imgCarte' src='../ImgCulture/jardinDesPlante.jpg'></div><div style='float:right; padding: 10px;'><b>Jardin des Plantes </b><br/> Jardin alpin, écologique, roseraie,   <br/>Partez à la découverte des onze jardins<br/> qui composent un Jardin des Plantes unique  <br/> Ville, Paris</div>";
 const sculpture =
-  "<div style='float:left'><img class ='imgCarte' src='../ImgMarker/chatParis.jpeg'></div><div style='float:right; padding: 10px;'><b>Chat de Geluck</b><br/>En ces temps de crise sanitaire<br/>voici une expo XXL en plein air gratuite de surcroît <br/>qui devrait vous offrir un grand bol d’art frais. <br/> Ville, Paris</div>";
+  "<div style='float:left'><img class ='imgCarte' src='../ImgCulture/chatParis.jpeg'></div><div style='float:right; padding: 10px;'><b>Chat de Geluck</b><br/>En ces temps de crise sanitaire<br/>voici une expo XXL en plein air gratuite de surcroît <br/>qui devrait vous offrir un grand bol d’art frais. <br/> Ville, Paris</div>";
+const biblio =
+  "<div style='float:left'><img class ='imgCarte' src='../ImgCulture/bilbio.jpg'></div><div style='float:right; padding: 10px;'><b>Bibliothèque Historique</b><br/>La Bibliothèque historique <br/>de la ville d paris est une bibliothèque publique<br/>spécialisée dans l'histoire de Paris  <br/> Ville, Paris</div>";
 
 // Our markers
 
 const markers1 = [
   //Nature
-  [vincenne, "Bois de boulogne", 48.8403499, 2.4363478, "nature"],
+  [vincenne, "Bois de vincenne", 48.8403499, 2.4363478, "nature"],
   [lourcq, "Canal de Lourcq", 48.935773, 2.3580232, "nature"],
   [maisonJardinage, "Maison Du Jardinage", 48.8362891, 2.3829078, "nature"],
   [mars, "Champs de Mars,", 48.8578883, 2.2974414, "nature"],
   [jardinLuxembourg, "Jardin du Luxembourg", 48.84687, 2.33717, "nature"],
+  [boulogne, "Bois de Boulogne", 48.8640928, 2.2482579, "nature"],
 
   //detente
   [dansLeNoir, "Dans le noir ", 48.8664533, 2.3442977, "detente"],
@@ -92,12 +108,15 @@ const markers1 = [
   [barZen, "Bar Zen", 48.8677184, 2.3353527, "detente"],
   [jacuzzi, "Jacuzzi", 48.896333, 2.291332, "detente"],
   [brocante, "Brocante", 48.848758697509766, 2.3785629272460938, "detente"],
+
   //food
   [piment, "Trois fois plus de piments", 48.8672804, 2.3545663, "food"],
   [Luz, "Luz verde", 48.8803935, 2.3376028, "food"],
   [paul, "Le 6 Paul Bert", 48.8511962890625, 2.3844802379608154, "food"],
   [grille, "Le Grillé", 48.86887741088867, 2.3358967304229736, "food"],
   [adar, "Adar", 48.87087631225586, 2.341618776321411, "food"],
+  [sezame, "Sezame", 48.945194244384766, 2.3518362045288086, "food"],
+
   //culture
   [parfum, "Musée du Parfum", 48.8714648, 2.3302801, "culturel"],
   [gallery, "Exposition", 48.84931182861328, 2.36824631690979, "culturel"],
@@ -116,6 +135,7 @@ const markers1 = [
     2.3112874031066895,
     "culturel",
   ],
+  [biblio, "Bibliotheque", 48.856529, 2.360995, "culturel"],
 ];
 
 /**
@@ -188,14 +208,12 @@ filterMarkers = function (category) {
 
     //
     if (marker.category === category || category.length === 0) {
-      document.getElementById("building").style.display = "none"; // fait disparaitre le loader
       marker.setVisible(true); // affichage marker
       bounds.extend(marker.getPosition()); // recup position
       map.setZoom(12); // niveau du zoom
       map.setMapTypeId("terrain"); // plan terrain ou satelitte
     } else if (category === "start") {
-      // re affichage de loader si aucune selection
-      document.getElementById("building").style.display = "flex"; // reaparation du loader
+      // reset affichage  si aucune selection
       marker.setVisible(true); // affichage marker
       bounds.extend(marker.getPosition());
       map.setZoom(12);
@@ -213,14 +231,14 @@ filterMarkers = function (category) {
 // appel de la function initalisation de la map
 initialize();
 
-// animation css
+// animation css selection element
 const titre = document.querySelector("h1");
 const txt = document.querySelector(".pHome");
 const btn = document.querySelector("#btnHome");
 const imgLaptop = document.querySelector(".laptop");
 const allItems = document.querySelectorAll("li");
 
-// lire la doc
+// regle css propre a la librairy GSAP
 const TL1 = new TimelineMax({ paused: true });
 TL1.from(titre, 1, { y: -100, opacity: 0 })
   .from(txt, 1, { opacity: 0 }, "-=0.4")
